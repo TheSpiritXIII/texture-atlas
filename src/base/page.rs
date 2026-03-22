361ab4f345ad5364fae8ac11db382b08beb3f111
Daniel Hrabovcak <4027687+TheSpiritXIII@users.noreply.github.com>
2026-03-22 18:19:42 -0400

    feat: add PageRect type, implement AtlasRect for Page and add docs

--- src/base/page.rs
+++ src/base/page.rs
@@ -1,17 +1,44 @@
 use crate::AtlasRect;
 
-pub trait AtlasPage<T: AtlasRect> {
+/// Represents a single page in a texture atlas.
+pub trait AtlasPage<T: AtlasRect>: AtlasRect {
+	/// The error type when adding a rect to the page.
 	type Error;
 
+	/// Creates a new page with the given maximum size.
 	fn new(width: u32, height: u32) -> Self;
+
+	/// Adds a new rect to the page at the given position. The given rect should not overlap with
+	/// any other rects previously pass into this function.
 	fn rect_add(&mut self, rect: &T, x: u32, y: u32) -> Result<(), Self::Error>;
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+/// References an axis aligned rect placed in a page.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct PageRect<T> {
+	/// The original rect.
+	rect: T,
+
+	/// The x-position where this rect is located in the bin.
+	x: u32,
+
+	/// The y-position where this rect is located in the bin.
+	y: u32,
+}
+
+/// A utility type that implements `AtlasPage`.
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Page<T: AtlasRect> {
-	pub width: usize,
-	pub height: usize,
-	pub rects: Vec<(T, usize, usize)>,
+	width: u32,
+	height: u32,
+	rect_list: Vec<PageRect<T>>,
+}
+
+impl<T: AtlasRect> Page<T> {
+	/// Returns the list of rects stored in this page.
+	pub fn rect_list(&self) -> &[PageRect<T>] {
+		&self.rect_list
+	}
 }
 
 impl<T: AtlasRect + Clone> AtlasPage<T> for Page<T> {
@@ -19,14 +46,28 @@ impl<T: AtlasRect + Clone> AtlasPage<T> for Page<T> {
 
 	fn new(width: u32, height: u32) -> Self {
 		Self {
-			width: width as usize,
-			height: height as usize,
-			rects: Vec::new(),
+			width,
+			height,
+			rect_list: Vec::new(),
 		}
 	}
 
 	fn rect_add(&mut self, rect: &T, x: u32, y: u32) -> Result<(), Self::Error> {
-		self.rects.push((rect.clone(), x as usize, y as usize));
+		self.rect_list.push(PageRect {
+			rect: rect.clone(),
+			x,
+			y,
+		});
 		Ok(())
+	}
+}
+
+impl<T: AtlasRect> AtlasRect for Page<T> {
+	fn width(&self) -> u32 {
+		self.width
+	}
+
+	fn height(&self) -> u32 {
+		self.height
 	}
 }
