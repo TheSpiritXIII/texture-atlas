1d4b905089c487dd037864ee7219cd9662f0c595
Daniel Hrabovcak <4027687+TheSpiritXIII@users.noreply.github.com>
2026-04-26 12:32:33 -0400

    feat: support size ranges for image generation

--- gen/src/generate.rs
+++ gen/src/generate.rs
@@ -1,7 +1,12 @@
 use image::RgbImage;
+use rand::RngExt;
 
-pub fn generate() -> RgbImage {
-	let mut image = RgbImage::new(128, 128);
+pub fn generate(min: (u32, u32), max: (u32, u32)) -> RgbImage {
+	let mut rng = rand::rng();
+	let width = rng.random_range(min.0..=max.0);
+	let height = rng.random_range(min.1..=max.1);
+
+	let mut image = RgbImage::new(width, height);
 	image.fill(255);
 	image
 }
--- gen/src/main.rs
+++ gen/src/main.rs
@@ -15,16 +15,36 @@ struct Cli {
 	/// How many images to generate.
 	#[arg(long)]
 	amount: u16,
+
+	/// The minimum width for generated images. The image width will be random between this and
+	/// `max_width`.
+	#[arg(long)]
+	min_width: u32,
+
+	/// The minimum height for generated images. The image height will be random between this and
+	/// `max_height`.
+	#[arg(long)]
+	min_height: u32,
+
+	/// The maximum width for generated images. The image width will be random between `min_width`
+	/// and this.
+	#[arg(long)]
+	max_width: u32,
+
+	/// The maximum height for generated images. The image height will be random between
+	/// `min_height` and this.
+	#[arg(long)]
+	max_height: u32,
 }
 
 fn main() -> io::Result<()> {
 	let cli = Cli::parse();
-	println!("output: {}", cli.output_dir.display());
 
 	fs::create_dir_all(&cli.output_dir)?;
 	for i in 0..cli.amount {
 		let output_path = cli.output_dir.join(format!("image_{}.png", i));
-		let image = generate::generate();
+		let image =
+			generate::generate((cli.min_width, cli.min_height), (cli.max_width, cli.max_height));
 		image.save(&output_path).unwrap();
 	}
 	Ok(())
